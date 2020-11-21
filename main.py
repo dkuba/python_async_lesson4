179e230c54dce3868e22563a14e0d83121debaad
dima <dkubarev88@gmail.com>
2020-11-22 00:33:00 +0300

    Save messages history to file.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import asyncio
+from time import strftime, localtime
+import aiofiles
 
 
-# Press the green button in the gutter to run the script.
+async def tcp_chat_sniffer():
+
+    reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
+    try:
+        while True:
+            async with aiofiles.open('log.txt', mode='a') as f:
+                data = await reader.readline()
+                out_string = f'[{strftime("%d.%m.%y %H:%M", localtime())}] {data.decode("utf-8")}'
+                print(f'[{strftime("%d.%m.%y %H:%M", localtime())}] {data.decode("utf-8")}')
+                await f.write(out_string)
+    except RuntimeError:
+        return
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ioloop = asyncio.get_event_loop()
+    tasks = [ioloop.create_task(tcp_chat_sniffer())]
+    wait_tasks = asyncio.wait(tasks)
+    ioloop.run_until_complete(wait_tasks)
