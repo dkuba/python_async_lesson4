a356b977500c3ac2c0b41dc3d8c7f96cd364a5af
dima <dkubarev88@gmail.com>
2020-11-29 18:37:43 +0300

    Setup logging.

--- chat_writer.py
+++ chat_writer.py
@@ -1,12 +1,30 @@
 import asyncio
 import config
+import logging
+
+
+logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 async def chat_writer(message):
-    _, writer = await asyncio.open_connection(config.app_config[config.HOST], 5050)
+    reader, writer = await asyncio.open_connection(config.app_config[config.HOST], 5050)
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
 
     writer.write(str(config.CHAT_TOKEN + '\n').encode())
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
     writer.write(f'{message}\n\n'.encode())
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
 
 if __name__ == '__main__':
     asyncio.run(chat_writer('!!!ТЕСТОВОЕ СООБЩЕНИЕ!!!'))
