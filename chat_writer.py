c185366163e6924b0abaa1bb8c4dfc43a65c63fc
dima <dkubarev88@gmail.com>
2020-11-30 17:52:20 +0300

    Refactor code, split by reusable functions.

--- chat_writer.py
+++ chat_writer.py
@@ -9,16 +9,58 @@ logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-async def chat_writer(message):
+async def register(nickname):
     reader, writer = await asyncio.open_connection(config.app_config[config.HOST], 5050)
 
     data = await reader.readline()
     logger.debug(data.decode("utf-8"))
 
-    writer.write(str(config.CHAT_TOKEN + '1\n').encode())
+    writer.write('\n'.encode())
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
+    writer.write(f'{nickname}\n'.encode())
+
+    data = await reader.readline()
+    config.CHAT_TOKEN = json.loads(data.decode("utf-8"))["account_hash"]
+
+    logger.debug(data.decode("utf-8"))
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def authorise():
+    reader, writer = await asyncio.open_connection(config.app_config[config.HOST], 5050)
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
+    writer.write(str(config.CHAT_TOKEN + '\n').encode())
 
     data = await reader.readline()
 
+    if not json.loads(data.decode("utf-8")):
+        print('введен неверный токен')
+        return
+
+    logger.debug(data.decode("utf-8"))
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
+
+async def submit_message(message):
+    reader, writer = await asyncio.open_connection(config.app_config[config.HOST], 5050)
+
+    data = await reader.readline()
+    logger.debug(data.decode("utf-8"))
+
+    writer.write(str(config.CHAT_TOKEN + '\n').encode())
+
+    data = await reader.readline()
     if not json.loads(data.decode("utf-8")):
         print('введен неверный токен')
         return
@@ -33,5 +75,14 @@ async def chat_writer(message):
     logger.debug(data.decode("utf-8"))
 
 
+async def main(nickname, message):
+
+    await register(nickname)
+
+    await authorise()
+
+    await submit_message(message)
+
 if __name__ == '__main__':
-    asyncio.run(chat_writer('!!!ТЕСТОВОЕ СООБЩЕНИЕ!!!'))
+    asyncio.run(main('David', '!!!HELLO!!!'))
+
